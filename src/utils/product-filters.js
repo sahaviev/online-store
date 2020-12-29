ef1025b9e31332ea18f2613c98c3c55eb756d6a5
Rail Sakhaviev <rsakhaviev@alfabank.ru>
2020-12-29 14:55:47 +0300

    1.0.33: rework filters[2]

--- src/utils/product-filters.js
+++ src/utils/product-filters.js
@@ -13,69 +13,54 @@ export const getProductsPriceRanges = (products) => {
   };
 };
 
-const checkEstateType = (filters, product) => {
-  const estateType = filters[`estate-type`];
-  return !estateType || estateType.length === 0 || estateType.length > 0 && estateType.includes(product.filters.type);
+const checkEstateType = (estateTypeFilter, productType) =>
+  !estateTypeFilter || estateTypeFilter.length === 0 || estateTypeFilter.length > 0 && estateTypeFilter.includes(productType);
+
+const checkEstateSquare = (minSquareFilter, productArea) => !minSquareFilter || productArea > Number(minSquareFilter);
+
+const checkEstateRoomsCount = (roomsFilter, productRoomsCount) => {
+  const exactCount = Number(roomsFilter) === productRoomsCount;
+  const fiveAndMore = roomsFilter === `five_and_more` && productRoomsCount >= 5;
+  return !roomsFilter || roomsFilter === `any` || exactCount || fiveAndMore;
 };
 
-const checkEstateSquare = (filters, product) => {
-  const minSquare = filters[`min-square`];
-  return !minSquare || product.filters.area > Number(minSquare);
+const checkLaptopType = (laptopType, productType) =>
+  !laptopType || laptopType.length === 0 || laptopType.length > 0 && laptopType.includes(productType);
+
+const checkLaptopRam = (ram, productRam) => {
+  return !ram || ram === `any` || Number(ram) === productRam;
 };
 
-const checkEstateRoomsCount = (filters, product) => {
-  const exactCount = Number(filters.rooms) === product.filters[`rooms-count`];
-  const fiveAndMore = filters.rooms === `five_and_more` && product.filters[`rooms-count`] >= 5;
-  return !filters.rooms || filters.rooms === `any` || exactCount || fiveAndMore;
+const checkLaptopScreenSize = (diagonal, productScreenSize) => {
+  return !diagonal || diagonal === `any` || Number(diagonal) === Math.floor(productScreenSize);
 };
 
-const checkLaptopType = (filters, product) => {
-  const laptopType = filters[`laptop-type`];
-  return !laptopType || laptopType.length === 0 || laptopType.length > 0 && laptopType.includes(product.filters.type);
+const checkLaptopCpu = (processor, productCpu) => {
+  return !processor || processor.length === 0 || processor.length > 0 && processor.includes(productCpu);
 };
 
-const checkLaptopRam = (filters, product) => {
-  return !filters.ram || filters.ram === `any` || Number(filters.ram) === product.filters[`ram-value`];
+const checkCameraType = (cameraType, productType) => {
+  return !cameraType || cameraType.length === 0 || cameraType.length > 0 && cameraType.includes(productType);
 };
 
-const checkLaptopScreenSize = (filters, product) => {
-  const {diagonal} = filters;
-  return !diagonal || diagonal === `any` || Number(diagonal) === Math.floor(product.filters[`screen-size`]);
+const checkCameraMatrix = (matrixResolution, productMatrix) => {
+  return !matrixResolution || matrixResolution === `any` || Math.floor(productMatrix) >= Number(matrixResolution);
 };
 
-const checkLaptopCpu = (filters, product) => {
-  const processor = filters[`laptop-processor`];
-  return !processor || processor.length === 0 || processor.length > 0 && processor.includes(product.filters[`cpu-type`]);
+const checkCameraVideo = (videoResolution, productVideoSupporting) => {
+  return !videoResolution || videoResolution === `any` || videoResolution === productVideoSupporting.toLowerCase();
 };
 
-const checkCameraType = (filters, product) => {
-  const cameraType = filters[`camera-type`];
-  return !cameraType || cameraType.length === 0 || cameraType.length > 0 && cameraType.includes(product.filters.type);
+const checkCarProductionYear = (productionYear, productProductionYear) => {
+  return !productionYear || productionYear === `any` || Number(productProductionYear) >= Number(productionYear);
 };
 
-const checkCameraMatrix = (filters, product) => {
-  const matrixResolution = filters[`resolution-matrix`];
-  return !matrixResolution || matrixResolution === `any` || Math.floor(product.filters[`matrix-resolution`]) >= Number(matrixResolution);
+const checkCarTransmission = (transmission, productTransmission) => {
+  return !transmission || transmission === `any` || transmission === productTransmission;
 };
 
-const checkCameraVideo = (filters, product) => {
-  const videoResolution = filters[`resolution-video`];
-  return !videoResolution || videoResolution === `any` || videoResolution === product.filters.supporting.toLowerCase();
-};
-
-const checkCarProductionYear = (filters, product) => {
-  const productionYear = filters[`production-year`];
-  return !productionYear || productionYear === `any` || Number(product.filters[`production-year`]) >= Number(productionYear);
-};
-
-const checkCarTransmission = (filters, product) => {
-  const {transmission} = filters;
-  return !transmission || transmission === `any` || transmission === product.filters.transmission;
-};
-
-const checkCarBodyType = (filters, product) => {
-  const bodyType = filters[`body-type`];
-  return !bodyType || bodyType.length > 0 && bodyType.includes(product.filters[`body-type`]);
+const checkCarBodyType = (bodyType, productBodyType) => {
+  return !bodyType || bodyType.length > 0 && bodyType.includes(productBodyType);
 };
 
 
@@ -96,28 +81,28 @@ export const filterProducts = (products, category, filters) => products.filter((
   switch (category) {
     case CategoryType.ESTATE:
       return (
-        checkEstateType(filters, product) &&
-        checkEstateSquare(filters, product) &&
-        checkEstateRoomsCount(filters, product)
+        checkEstateType(filters[`estate-type`], product.filters.type) &&
+        checkEstateSquare(filters[`min-square`], product.filters.area) &&
+        checkEstateRoomsCount(filters.rooms, product.filters[`rooms-count`])
       );
     case CategoryType.LAPTOPS:
       return (
-        checkLaptopType(filters, product) &&
-        checkLaptopRam(filters, product) &&
-        checkLaptopScreenSize(filters, product) &&
-        checkLaptopCpu(filters, product)
+        checkLaptopType(filters[`laptop-type`], product.filters.type) &&
+        checkLaptopRam(filters.ram, product.filters[`ram-value`]) &&
+        checkLaptopScreenSize(filters.diagonal, product.filters[`screen-size`]) &&
+        checkLaptopCpu(filters[`laptop-processor`], product.filters[`cpu-type`])
       );
     case CategoryType.CAMERA:
       return (
-        checkCameraType(filters, product) &&
-        checkCameraMatrix(filters, product) &&
-        checkCameraVideo(filters, product)
+        checkCameraType(filters[`camera-type`], product.filters.type) &&
+        checkCameraMatrix(filters[`resolution-matrix`], product.filters[`matrix-resolution`]) &&
+        checkCameraVideo(filters[`resolution-video`], product.filters.supporting)
       );
     case CategoryType.CARS:
       return (
-        checkCarProductionYear(filters, product) &&
-        checkCarTransmission(filters, product) &&
-        checkCarBodyType(filters, product)
+        checkCarProductionYear(filters[`production-year`], product.filters[`production-year`]) &&
+        checkCarTransmission(filters.transmission, product.filters.transmission) &&
+        checkCarBodyType(filters[`body-type`], product.filters[`body-type`])
       );
     default:
       return true;
